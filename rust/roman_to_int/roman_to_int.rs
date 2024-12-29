03b024aa5adf2b213935bfccef1df92d15f4bcd9
Cali LaFollett <cali.lafollett@gmail.com>
2024-12-29 02:35:37 -0500

    impl rust `roman_to_int` alg

--- rust/roman_to_int/roman_to_int.rs
+++ rust/roman_to_int/roman_to_int.rs
@@ -18,9 +18,33 @@ fn main() {
 }
 
 pub fn roman_to_int(s: String) -> i32 {
-    for (i, c) in s.chars().enumerate() {
-        println!("i: {}, c: {}", i, c);
+    let mut val = 0;
+    let mut prev_char: char = '\0';
+
+    for (_i, c) in s.chars().enumerate() {
+        let mut token_val = match c {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
+        };
+
+        // Subtract twice the value of the token to account for the previous token value being added
+        if prev_char == 'I' && (c == 'V' || c == 'X') {
+            token_val -= 2;
+        } else if prev_char == 'X' && (c == 'L' || c == 'C') {
+            token_val -= 20;
+        } else if prev_char == 'C' && (c == 'D' || c == 'M') {
+            token_val -= 200;
+        }
+
+        prev_char = c;
+        val += token_val;
     }
 
-    return 0;
+    return val;
 }
