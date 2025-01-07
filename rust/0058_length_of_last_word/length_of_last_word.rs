4b8fdeead19a8f91479bdbb262803ee781e2d8d7
Cali LaFollett <cali.lafollett@gmail.com>
2025-01-06 20:48:04 -0500

    added `length_of_last_word`

---
+++ rust/0058_length_of_last_word/length_of_last_word.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let test_cases: Vec<(&str, i32)> = vec![
+        ("word", 4),
+        ("Hello World", 5),
+        ("   fly me   to   the moon  ", 4),
+        ("luffy is still joyboy", 6),
+    ];
+
+    for (val, expected) in test_cases {
+        let result = length_of_last_word(val.to_string());
+        println!(
+            "string: {}, result: {}, expected: {}",
+            val, result, expected
+        );
+        assert!(result == expected);
+    }
+}
+
+pub fn length_of_last_word(s: String) -> i32 {
+    let mut len: i32 = 0;
+    let mut iter = s.bytes().rev();
+
+    while let Some(c) = iter.next() {
+        if c != b' ' {
+            len += 1;
+            continue;
+        }
+
+        if c == b' ' && len > 0 {
+            return len;
+        }
+    }
+
+    return len;
+}
